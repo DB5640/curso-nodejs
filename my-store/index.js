822baa7f90a0201cfe1c96d35f14e9e32fa06b51
DB5640 <122465471+DB5640@users.noreply.github.com>
2025-03-31 13:45:22 +0000

    Separacion de responsabilidades

--- my-store/index.js
+++ my-store/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes')
 
 
 const app = express();
@@ -9,15 +9,9 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
-app.get('/products', (req, res) => {
-  const products = Array.from({ length: 10 }, () => ({
-    id: faker.random.uuid(),
-    name: faker.commerce.productName(),
-    price: parseInt(faker.commerce.price()),
-    image: faker.image.imageUrl()
-  }));
-  res.json(products);
-});
+
+routerApi(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
