f6d6b90c0050cc1cb6fa0d21377547ea8202b643
DB5640 <david.b5640@gmail.com>
2025-05-01 14:43:07 -0500

    refactor: update product service and router with improved structure and validation
    - Removed unused eslint dependency and added standard for code style.
    - Refactored products.router.js to include validation and improved error handling.
    - Updated products.service.js for better readability and consistency in async methods.
    - Enhanced product creation and retrieval logic with clearer error messages.

--- my-store/routes/proucts.router.js
+++ my-store/routes/proucts.router.js
@@ -1,21 +1,25 @@
-
+/* eslint-disable no-unused-vars */
 const express = require('express')
 
-const ProductsService = require('../services/products.service')
+const ProductsService = require('../services/products.service.js')
+const validatorHandler = require('../middlewares/validatorHandler.js')
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema
+} = require('../schemas/product.dto.js')
 
-const router = express.Router();
-const service = new ProductsService();
+const router = express.Router()
+const service = new ProductsService()
 
-router.get('/',
-  async (req, res) => {
-    const products = await service.find();
-    res.json(products);
-  }
-);
+router.get('/', async (req, res) => {
+  const products = await service.find()
+  res.json(products)
+})
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     const product = await service.findOne(id)
     res.status(200).json(product)
   } catch (err) {
@@ -24,7 +28,7 @@ router.get('/:id', async (req, res, next) => {
 })
 
 router.post('/', async (req, res) => {
-  const body = req.body;
+  const body = req.body
   const newProduct = await service.create(body)
   res.status(201).json({
     message: 'created',
@@ -34,19 +38,19 @@ router.post('/', async (req, res) => {
 
 router.patch('/:id', async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const body = req.body;
+    const { id } = req.params
+    const body = req.body
     const product = await service.update(id, body)
-    res.json({message: 'Product updated', product})
+    res.json({ message: 'Product updated', product })
   } catch (err) {
     next(err)
   }
 })
 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   const prtDeleted = await service.delete(id)
-  res.json({message: "producto eliminado", prtDeleted})
+  res.json({ message: 'producto eliminado', prtDeleted })
 })
 
-module.exports = router;
+module.exports = router
--- my-store/services/products.service.js
+++ my-store/services/products.service.js
@@ -1,80 +1,73 @@
-const { faker } = require('@faker-js/faker')
-const boom = require('@hapi/boom')
+const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
-
-  constructor(){
+  constructor() {
     this.products = [];
     this.generate();
   }
 
-  generate(){
-    const limit = 100
-    Array.from({ length: limit },
-      () => (
-        this.products.push({
-          id: faker.string.ulid(),
-          name: faker.commerce.productName(),
-          price: parseInt(faker.commerce.price()),
-          image: faker.image.url(),
-          isBlock: faker.datatype.boolean(0.3)
-        })
-      )
+  generate() {
+    const limit = 100;
+    Array.from({ length: limit }, () =>
+      this.products.push({
+        id: faker.string.ulid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price()),
+        image: faker.image.url(),
+        isBlock: faker.datatype.boolean(0.3),
+      }),
     );
   }
 
-  async create(data){
+  async create(data) {
     const newProduct = {
       id: faker.string.ulid(),
-      ...data
-    }
-    this.products.push(newProduct)
-    return newProduct
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
   }
 
-  async find(){
-    return new Promise((resolve, reject)=>{
-      setTimeout(()=>{
-        resolve(this.products)
-      }, 200)
-    })
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 200);
+    });
   }
 
-  async findOne(id){
-
-    const product = this.products.find(product => product.id === id)
-    if(!product){
-      throw boom.notFound('Product not found')
+  async findOne(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      throw boom.notFound('Product not found');
     }
-    if(product.isBlock){
-      throw boom.conflict('product is blocked')
+    if (product.isBlock) {
+      throw boom.conflict('product is blocked');
     }
-    return product
+    return product;
   }
 
-  async update(id, changes){
-    const index = this.products.findIndex(product => product.id === id)
-    if(index === -1){
+  async update(id, changes) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
       throw boom.notFound('Product not found');
     }
     const product = this.products[index];
     this.products[index] = {
       ...product,
-      ...changes
-    }
+      ...changes,
+    };
     return this.products[index];
   }
 
-  async delete(id){
-    const index = this.products.findIndex(product => product.id === id)
-    if(index === -1){
-      throw boom.notFound('Product not found')
+  async delete(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw boom.notFound('Product not found');
     }
-    return this.products.splice(index, 1)
+    return this.products.splice(index, 1);
   }
-
-
-
 }
 
 module.exports = ProductsService;
